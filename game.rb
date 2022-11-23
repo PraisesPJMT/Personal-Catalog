cf4a9664892b07d198fa1b0b5da21b6e7d934deb
shaqdeff <shaquillendunda@gmail.com>
2022-11-23 16:27:33 +0300

    Fix linter errors

--- game.rb
+++ game.rb
@@ -4,12 +4,13 @@ require './item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize (multiplayer, last_played_at)
+  def initialize(multiplayer, last_played_at)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+    super
   end
-  
-# return true if parent's method returns true AND if last_played_at is older than 2 years
+
+  # return true if parent's method returns true AND if last_played_at is older than 2 years
   def can_be_archived?
     super && Date.today.year - Date.parse(@last_played_at).year > 2
   end
