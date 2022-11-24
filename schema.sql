6894ee4e8981614456d086f0c35165daa37d601d
PraisesPJMT <praisesmusa@outlook.com>
2022-11-24 11:12:45 +0100

    Add schema for creating `Label` table in database

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
+-- Creating a database called 'catalog_of_things'
+CREATE DATABASE catalog_of_things;
+
 --  create a games table
 CREATE TABLE games (
  id SERIAL PRIMARY KEY,
@@ -21,6 +24,26 @@ CREATE TABLE label (
   item_id INT FOREIGN KEY REFERENCES item(id),
 	title TEXT,
 	color TEXT
+);
+
+-- create authors table
+CREATE TABLE authors (
+ id SERIAL PRIMARY KEY,
+ first_name VARCHAR(150) NOT NULL,
+ last_name VARCHAR(150) NOT NULL,
+ item_id INT REFERENCES item(id)
+);
+/* Personal Item Catalogue Database
+*/
+
+
+/* Creating 'Genre' table in 'catalog_of_things' database
+   Table has columns: id, and name
+   Constraints: id as the primary key
+*/
+CREATE TABLE genre(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100)
 );
 
 -- create authors table
