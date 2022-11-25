626fffbd59648f1600f295a489663f5b0895aa78
benardop <benard.oc.p@gmail.com>
2022-11-25 17:30:31 +0300

    Update the schema script file to create tables for classes

--- schema.sql
+++ schema.sql
@@ -6,6 +6,21 @@ CREATE TABLE genre(
     name VARCHAR(100)
 );
 
+---Label table--
+CREATE TABLE label (
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	title TEXT,
+	color TEXT
+);
+
+-- authors table
+CREATE TABLE author (
+ id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+ first_name VARCHAR(150) NOT NULL,
+ last_name VARCHAR(150) NOT NULL,
+);
+
+
 CREATE TABLE music_album(
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     genre_id      INT,
@@ -14,7 +29,7 @@ CREATE TABLE music_album(
     name    VARCHAR(100),
     publish_date  DATE,
     archived      BOOLEAN,
-    on_spotify VARCHAR(100),
+    on_spotify    BOOLEAN
 
     CONSTRAINT fk_genre_id FOREIGN KEY(genre_id) REFERENCES genre(id),
 
@@ -27,31 +42,17 @@ CREATE TABLE games (
  id SERIAL PRIMARY KEY,
  multiplayer INT,
  last_played_at DATE NOT NULL,
- item_id INT REFERENCES item(id)
  publish_date DATE NOT NULL,
  archived BOOLEAN
-);
-
--- authors table
-CREATE TABLE authors (
- id SERIAL PRIMARY KEY,
- first_name VARCHAR(150) NOT NULL,
- last_name VARCHAR(150) NOT NULL,
- item_id INT REFERENCES item(id)
-);
-
----Label table--
-CREATE TABLE label (
-	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  item_id INT FOREIGN KEY REFERENCES item(id),
-	title TEXT,
-	color TEXT
 );
 
 ---Book class table--
 CREATE TABLE book (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  item_id INT FOREIGN KEY REFERENCES item(id),
+  author_id INT FOREIGN KEY REFERENCES author(id),
+  genre_id INT FOREIGN KEY REFERENCES genre(id),
+  label_id INT FOREIGN KEY REFERENCES label(id),
   publisher TEXT,
   cover_state TEXT,
+  archived BOOLEAN
 );
