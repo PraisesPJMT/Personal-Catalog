5f1b219a13307e6c43be195f66084a76b22b9f6d
shaqdeff <shaquillendunda@gmail.com>
2022-11-25 14:19:44 +0300

    Remove comments

--- schema.sql
+++ schema.sql
@@ -1,27 +1,10 @@
-/* Personal Item Catalogue Database
-   Tables: Book, Game, MusicAlbum, Genre, Label, & Author
-*/
-
 -- Creating a database called 'catalog_of_things'
 CREATE DATABASE catalog_of_things;
 
-/* Creating 'Genre' table in 'catalog_of_things' database
-   Table has columns: id, and name
-   Constraints: id as the primary key
-*/
 CREATE TABLE genre(
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100)
 );
-
-/* Creating 'MusicAlbum' table in 'catalog_of_things' database
-   Table has columns: id, genre_id, author_id, label_id, name,
-                      publish_date, archived, and on_spotify
-   Constraints: id as the primary key
-                genre_id linked to id from genre table
-                author_id linked to id from author table
-                label_id linked to id from label table
-*/
 
 CREATE TABLE music_album(
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
