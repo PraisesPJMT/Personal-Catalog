54e226099a109e281bc5ea521fcdd416c443e3e4
shaqdeff <shaquillendunda@gmail.com>
2022-11-25 05:58:15 +0300

    Create tables to define game and author classes structure

--- schema.sql
+++ schema.sql
@@ -39,3 +39,20 @@ CREATE TABLE music_album(
 
     CONSTRAINT fk_label_id FOREIGN KEY(label_id) REFERENCES label(id)
 );
+--  create a games table
+CREATE TABLE games (
+ id SERIAL PRIMARY KEY,
+ multiplayer INT,
+ last_played_at DATE NOT NULL,
+ item_id INT REFERENCES item(id)
+ publish_date DATE NOT NULL,
+ archived BOOLEAN
+);
+
+-- create authors table
+CREATE TABLE authors (
+ id SERIAL PRIMARY KEY,
+ first_name VARCHAR(150) NOT NULL,
+ last_name VARCHAR(150) NOT NULL,
+ item_id INT REFERENCES item(id)
+);
