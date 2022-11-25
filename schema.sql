70906491de3cf3dcd0f76856291a37558b3f471c
shaqdeff <shaquillendunda@gmail.com>
2022-11-25 14:22:59 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -33,6 +33,8 @@ CREATE TABLE authors (
  last_name VARCHAR(150) NOT NULL,
  item_id INT REFERENCES item(id)
 );
+-- Creating a database called 'catalog_of_things'
+CREATE DATABASE catalog_of_things;
 
 CREATE TABLE genre(
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -55,4 +57,37 @@ CREATE TABLE music_album(
 
     CONSTRAINT fk_label_id FOREIGN KEY(label_id) REFERENCES label(id)
 );
+--  create a games table
+CREATE TABLE games (
+ id SERIAL PRIMARY KEY,
+ multiplayer INT,
+ last_played_at DATE NOT NULL,
+ item_id INT REFERENCES item(id)
+ publish_date DATE NOT NULL,
+ archived BOOLEAN
+);
 
+-- create authors table
+CREATE TABLE authors (
+ id SERIAL PRIMARY KEY,
+ first_name VARCHAR(150) NOT NULL,
+ last_name VARCHAR(150) NOT NULL,
+ item_id INT REFERENCES item(id)
+);
+
+
+---create Book class table--
+CREATE TABLE book (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  item_id INT FOREIGN KEY REFERENCES item(id),
+  publisher TEXT,
+  cover_state TEXT,
+);
+
+---create Label table--
+CREATE TABLE label (
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  item_id INT FOREIGN KEY REFERENCES item(id),
+	title TEXT,
+	color TEXT
+);
